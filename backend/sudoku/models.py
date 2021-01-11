40f68bc8c59cbea2b18681ac6c1e1e1f6db4cf88
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2021-01-10 16:46:13 -0800

    cleaned up extra spaces and comment

--- backend/sudoku/models.py
+++ backend/sudoku/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 def board_default():
     return {'board': []}
-# Create your models here.
+
 class Sudoku(models.Model):
     title = models.CharField(max_length=120)
     board = models.JSONField(default = board_default)
-
 
     def _str_(self):
         return self.title
