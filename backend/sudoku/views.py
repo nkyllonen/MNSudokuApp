ea14cc4027a9c3a000d4fe0afe3da597fea87a13
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2021-01-21 16:10:27 -0800

    creating sudoku view

--- backend/sudoku/views.py
+++ backend/sudoku/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import SudokuSerializer
+from .models import Sudoku
 
 # Create your views here.
+class SudokuView(viewsets.ModelViewSet):
+    serializer_class = SudokuSerializer
+    queryset = Sudoku.objects.all()  # represents all objects (for querying)
