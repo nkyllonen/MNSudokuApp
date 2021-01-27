8f39ac4bef2196f7f4b4883189afe44995655aa0
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2021-01-26 22:17:31 -0800

    added jquery and popper + displaying 3x3 grid of buttons

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,10 +1,28 @@
+// import { Button } from 'bootstrap'
 import React, { Component } from 'react'
+import { Table,Button } from 'reactstrap'
 
 export default class Board extends Component {
     render() {
+        const rows = [];
+        for (let y=0; y<3; y++){
+            const cols = [];
+            for (let x=0; x<3; x++) {
+                cols.push(<Button>#</Button>);
+            }
+
+            rows.push(<tr>{ cols }</tr>);
+        }
+
         return (
             <div>
                 <h2>BOARD</h2>
+
+                <Table bordered>
+                    <tbody>
+                        { rows }
+                    </tbody>
+                </Table>
             </div>
         )
     }
